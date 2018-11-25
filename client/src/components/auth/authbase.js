c56504dc4c5ee0d4d57f432d53c1a1cd97b75027
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-11-25 23:10:56 +0500

    Client Side Setup

---
+++ client/src/components/auth/authbase.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class AuthBase extends Component {
+  render() {
+    return (
+      <div className="page-ath-wrap">
+      <div className="page-ath-content">
+        <div className="page-ath-header"><a href="./" className="page-ath-logo"><h1 style={{fontSize: 50}}>SimpliCron</h1></a></div>
+        
+        { this.props.children }
+
+        <div className="page-ath-footer">
+            <ul className="footer-links">
+              <li><a href="regular-page.html">Privacy Policy</a></li>
+              <li><a href="regular-page.html">Terms</a></li>
+              <li>© 2018 SimpliCron.</li>
+            </ul>
+          </div>
+        </div>
+        <div className="page-ath-gfx">
+          <div className="w-100 d-flex justify-content-center">
+            <div className="col-md-8 col-xl-5"><img src="images/ath-gfx.png" alt="sideImage" /></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AuthBase;
--- client/src/containers/auth/signup.js
+++ client/src/containers/auth/signup.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-
+import AuthBase from "./../../components/auth/authbase"
+import { Link } from 'react-router-dom'
 class SignUp extends Component {
   render() {
     return (
-      <div>
-        <h1>Sign Up</h1>
-      </div>
+      <AuthBase>
+         <div className="page-ath-form">
+            <h2 className="page-ath-heading">Sign up <small>Create New Account</small></h2>
+            <form action="#">
+              <div className="input-item"><input type="text" placeholder="Your Email" className="input-bordered" /></div>
+              <div className="input-item"><input type="password" placeholder="Password" className="input-bordered" /></div>
+              <div className="input-item"><input type="password" placeholder="Repeat Password" className="input-bordered" /></div>
+              {/* <div class="input-item text-left"><input class="input-checkbox input-checkbox-md" id="term-condition" type="checkbox"><label for="term-condition">I agree to TokenWiz’s <a href="regular-page.html">Privacy Policy</a> &amp; <a href="regular-page.html"> Terms.</a></label></div> */}
+              <button className="btn btn-primary btn-block">Create Account</button>
+            </form>
+            {/* <div class="sap-text"><span>Or Sign Up With</span></div>
+                    <ul class="row guttar-20px guttar-vr-20px">
+                        <li class="col"><a href="#" class="btn btn-outline btn-dark btn-facebook btn-block"><em class="fab fa-facebook-f"></em><span>Facebook</span></a></li>
+                        <li class="col"><a href="#" class="btn btn-outline btn-dark btn-google btn-block"><em class="fab fa-google"></em><span>Google</span></a></li>
+                    </ul>
+                    <div class="gaps-2x"></div>
+                    <div class="gaps-2x"></div> */}
+            <div className="form-note">Already have an account ? <Link to="/"> <strong>Sign in instead</strong></Link></div>
+          </div>
+    </AuthBase>
     );
   }
 }
